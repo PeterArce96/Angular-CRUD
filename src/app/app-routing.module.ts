81add4761fad829ce7e2d515dc80d27efda0ff29
PeterArce96 <parce0496@gmail.com>
2022-08-08 16:34:09 -0500

    routes,components, crearCliente, boton nuevo cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'clientes', component: ListadoClientesComponent},
+  {path: 'crear-cliente',component: CrearClienteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ListadoClientesComponent,
+    CrearClienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
